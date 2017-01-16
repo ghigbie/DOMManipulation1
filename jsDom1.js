474eb2c827df670c48f65f69bdf6abdae969de4d
George Higbie <gehigbie@gmail.com>
2017-01-16 01:35:11 -0700

    fixed problem with js loading

--- jsDom1.js
+++ jsDom1.js
@@ -1,7 +1,10 @@
 //created in command line
 var h1 = document.querySelector("h1");
 
-h1.style.color = "blue";
-h1.style.border = "5px solid orange";
+h1.style.color = "red";
+h1.style.border = "5px solid pink";
 
 //we should add classes in javascript to turn these on in CSS
+
+var tag = document.querySelector("h1");
+tag.classList.add("blue");
