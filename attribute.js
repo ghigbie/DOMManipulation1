0349b7c4d50c469df4b96af0e95a25db76aaf0fe
George Higbie <gehigbie@gmail.com>
2017-01-16 20:07:25 -0700

    added elements and more js lines

--- attribute.js
+++ attribute.js
@@ -1,1 +1,9 @@
 //created in terminal`
+var link = document.querySelector("a");
+link.getAttribute("href"); ///www.google.com
+
+link.setAttribute("href", "www.dogs.com");
+
+var img = document.querySelector("img");
+img.setAttribute("src", "corgi.png");
+
